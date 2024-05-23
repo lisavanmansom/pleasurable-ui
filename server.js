ed39e615014217ddc68b0dd1d1e1ec80b789a3c5
Hadil24A <hadil.ahmed@hva.nl>
2024-05-23 18:53:52 +0200

    Homepage mouse animation

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log('Hier komt jullie server')
+// 1. OPZETTEN VAN DE WEBSERVER
+
+import express, { request } from "express"
+
+import fetchJson from "./helpers/fetch-json.js"
+
+let app = express()
+
+app.set("view engine", "ejs")
+
+app.set("views", "./views")
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: true }))
+
+// 2. ROUTES
+
+app.get("/", function (request, response) {
+    response.render("homepage", {})
+  })
+  
+// 3. START DE WEBSERVER
+
+app.set("port", process.env.PORT || 8000)
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`)
+})
